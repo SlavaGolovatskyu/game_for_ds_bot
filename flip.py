1ae360ba86fe634de7a5dd57e11aff13b13965ca
Slava <61500332+SlavaGolovatskyu@users.noreply.github.com>
2020-11-15 16:00:32 +0200

    Update flip.py

--- flip.py
+++ flip.py
@@ -1,16 +1,23 @@
 # DISCORD.PY // FOR COGS 
 import random, discord
-@commands.command()
-async def flip(self, ctx, member: discord.Member, get: str = None, money: int = None):
-  h = random.randint(0, 1)
-  list_for_game = ['Орел', 'Решка']
-  if get == 'Орел':
-    info = [0, 1]
-  elif get == 'Решка':
-    info = [1, 0]
-  ctx_author, member_men = info[0], info[1]
-  win = list_for_game[h]
-  if win == 'Орел':
-    await ctx.send(str(list_for_game[ctx_author]) + f' Победил первый игрок {ctx.author.name}')
-  else:
-    await ctx.send(str(list_for_game[member_men]) + f' Победил второй игрок {member.name}'
+class user(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+  @commands.command()
+  async def flip(self, ctx, member: discord.Member, get: str = None, money: int = None):
+    h = random.randint(0, 1)
+    list_for_game = ['Орел', 'Решка']
+    if get == 'Орел':
+      info = [0, 1]
+    elif get == 'Решка':
+      info = [1, 0]
+    ctx_author, member_men = info[0], info[1]
+    win = list_for_game[h]
+    if win == 'Орел':
+      await ctx.send(str(list_for_game[ctx_author]) + f' Победил первый игрок {ctx.author.name}')
+    else:
+      await ctx.send(str(list_for_game[member_men]) + f' Победил второй игрок {member.name}')
+
+def setup(bot):
+    bot.add_cog(user(bot))
